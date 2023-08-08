86156f9f0c6d257c41a6cafd485c1ed9235c72e2
hee462 <a01080809887@gmail.com>
2023-08-08 17:32:21 +0900

    upload

---
+++ react-004-bbs/src/data/BBsDto.js
@@ -0,0 +1,14 @@
+export const BBsDto = {
+  b_seq: 0,
+  b_title: "",
+  b_content: "",
+  b_nickname: "",
+  b_password: "",
+  b_ccode: "",
+  b_date: "",
+  viewcount: 0,
+
+  b_image: "",
+  b_origin_image: "",
+  m_files: [],
+};
